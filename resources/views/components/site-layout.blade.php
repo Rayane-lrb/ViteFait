73c58ae47fa8e1ff02609f16709d069e8b137913
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-09 10:05:09 +0200

    FEATURE: styling of site-layout

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -5,35 +5,37 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Document</title>
 </head>
 <body>
 <header>
-    <nav class="w-cover bg-red-600">
-        <a href="" class="mr-auto">Logo</a>
-        <a href="">Article</a>
-        <a href="">Faq</a>
-        <a href="">Category</a>
+    <nav class="flex flex-row w-cover gap-6 items-center px-10 py-4 bg-[#2E3440] text-xl text-gray-500 font-bold">
+        <a href="" class="mr-auto hover:text-[#E06020]">Logo</a>
+        <a href="" class="hover:text-[#E06020] transition">Article</a>
+        <a href="" class="hover:text-[#E06020] transition">Faq</a>
+        <a href="" class="hover:text-[#E06020] transition">Category</a>
         @auth
-            <a>Profile</a>
+            <a class="hover:text-[#E06020] transition">Profile</a>
+            <a class="hover:text-[#E06020] transition">Logout</a>
         @endauth
         <div>
-            <a href="">Login</a>
+            <a href="" class="bg-[#E06020] text-white px-4 py-1.5 rounded hover:bg-[#B04010] transition">Login</a>
         </div>
     </nav>
 </header>
 <main>
     {{$slot}}
 </main>
-<footer class="flex row">
+<footer class="flex flex-row bg-[#5B6470] justify-between items-center bg-orange-400 text-white px-10 py-1.5">
     <div>
-        <p>2026 @copyright</p>
+        <p class="text-sm text-gray-300">2026 @copyright</p>
     </div>
     <div>
-        <h1>Contact</h1>
-        <p>vitefait@gmail.com</p>
-        <p>+32 000 00 00</p>
-        <p>Bruxelles, Belgique</p>
+        <h1 class="font-semibold">Contact</h1>
+        <p class="text-sm text-gray-300">vitefait@gmail.com</p>
+        <p class="text-sm text-gray-300">+32 000 00 00</p>
+        <p class="text-sm text-gray-300">Bruxelles, Belgique</p>
     </div>
 </footer>
 </body>
