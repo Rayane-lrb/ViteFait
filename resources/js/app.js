79260d80f9a07b4b3abd1ca862145fe73290455d
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-10 08:30:33 +0200

    FEATURE: border color change on faq toggle

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,10 @@ window.Alpine = Alpine;
 
 Alpine.start();
 window.ToggleFaq = function(element) {
+    element.parentElement.classList.toggle('border-orange-500');
+
     element.nextElementSibling.classList.toggle('hidden');
+
     const icon = element.querySelector('img');
     icon.classList.toggle('rotate-45');
 }
