eb389c2cf2d9fb8589d060546d0dfe235057ad8c
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-10 08:35:04 +0200

    FEATURE: creation category controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 //USER
@@ -14,8 +15,10 @@ Route::get('/dashboard', function () {
 Route::get('/articles', [App\Http\Controllers\Admin\ArticleController::class, 'index'])->name('articles.index');
 Route::get('/articles/{id}', [App\Http\Controllers\Admin\ArticleController::class, 'show'])->name('article.show');
 
+
 Route::get('/faq', [\App\Http\Controllers\FaqController::class, 'index']);
 
+Route::get('/category', [\App\Http\Controllers\CategoryController::class, 'index']);
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
