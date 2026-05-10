2d5464ff408b98185f5d49fb4de9a2991c3a7dd6
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-10 22:46:11 +0200

    FEATURE: styling faq with category display

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -7,6 +7,7 @@
         <section class=" flex justify-center flex-col items-center p-4 w-full max-w-2xl">
         @foreach($faqs as $faq)
             <div class="bg-[#243447] mb-10 w-full border border-gray-400 rounded mr-auto h-15 p-4 cursor-pointer transition h-auto">
+                <p class="bg-orange-500/15 text-orange-400 rounded-full w-auto text-center min-w-auto max-w-25 mb-1">{{$faq->category->name}}</p>
                 <a onclick="ToggleFaq(this)" class="flex flex-row font-bold text-lg pointer">{{$faq->question}} <img src="images/toggle_icon.svg"
                                                                                                                  alt="toggle icon" class="ml-auto transition"></a>
                 <div class="answer hidden text-orange-300 transition">
