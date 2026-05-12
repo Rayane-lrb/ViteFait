cac93a9f83c3721f871efc9bfd0a1cd4a083bd6c
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-12 21:25:49 +0200

    FEATURE: register route + controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 //Public pages
 Route::get('/', function () {return view('welcome');});
-
+Route::get('/register', [RegisteredUserController::class, 'create']);
 
 //User pages
 Route::get('/dashboard', function () {return view('userzone.dashboard');})->middleware(['auth', 'verified'])->name('dashboard');
