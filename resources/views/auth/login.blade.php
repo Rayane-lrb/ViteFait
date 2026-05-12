c2b9b7c601c924b1e5b195460074e066e53ff1ac
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-12 23:37:08 +0200

    FIX: input names in login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,12 +6,12 @@
                 @csrf
                 <div class="flex flex-col gap-4 justify-center items-center">
                     <div class="flex flex-col gap-1.5 min-w-50 w-1/3">
-                        <label for="" class="text-[#9ca3af] text-xs font-medium">Adresse e-mail</label>
-                        <input type="email" class="bg-[#1c1f27] border border-[#2a2d35] rounded text-white">
+                        <label for="email" class="text-[#9ca3af] text-xs font-medium">Adresse e-mail</label>
+                        <input type="email" name="email" class="bg-[#1c1f27] border border-[#2a2d35] rounded text-white">
                     </div>
                     <div class="flex flex-col gap-1.5 min-w-50 w-1/3">
-                        <label for="" class="text-[#9ca3af] text-xs font-medium rounded">Mot de passe</label>
-                        <input type="password" class="bg-[#1c1f27] border border-[#2a2d35] rounded text-white">
+                        <label for="password" class="text-[#9ca3af] text-xs font-medium rounded">Mot de passe</label>
+                        <input type="password" name="password" class="bg-[#1c1f27] border border-[#2a2d35] rounded text-white">
                     </div>
                     <button class="text-white border border-[#2a2d35] p-3 w-1/3 rounded-md font-semibold hover:bg-[#22252f] hover:border-[#3a3d45]">Se connecter</button>
                     <div class="flex flex-row mb-20 gap-1">
