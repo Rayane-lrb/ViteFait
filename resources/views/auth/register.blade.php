38f01c6ec48345b12d9f4fedc4f9f9fc49cddb3b
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-12 21:26:10 +0200

    STYLE: register page styling

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,40 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<x-site-layout>
+    <div class="flex flex-row w-full min-h-screen">
+        <section class="bg-[#1c1f27] flex flex-col flex-wrap justify-around w-2/5 p-10 border-r border-[#2a2d35]">
+            <div>
+                <h3 class="text-white font-semibold text-2xl">Vite Fait</h3>
+            </div>
+            <div>
+                <h1 class="text-white text-4xl font-semibold">Déménagement</h1>
+                <h1 class="text-[#e87722] text-4xl font-semibold">rapide et sécurisé.</h1>
+            </div>
+            <div class="mb-20">
 
-        <!-- Name -->
-        <div>
-            <x-breeze.input-label for="name" :value="__('Name')" />
-            <x-breeze.text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-breeze.input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-breeze.input-label for="email" :value="__('Email')" />
-            <x-breeze.text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-breeze.input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-breeze.input-label for="password" :value="__('Password')" />
-
-            <x-breeze.text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-breeze.input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-breeze.input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-breeze.text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-breeze.input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-breeze.primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-breeze.primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+            </div>
+        </section>
+        <section class="bg-[#15171e] w-3/5 p-10 flex flex-col justify-center">
+            <h1 class="text-white text-2xl font-semibold mb-1">Créer mon compte</h1>
+            <form action="" class="flex flex-col gap-4">
+                @csrf
+                <div class="flex flex-col gap-4 justify-center items-center">
+                    <div class="flex flex-col gap-1.5 min-w-50 w-1/3">
+                            <label for="" class="text-[#9ca3af] text-xs font-medium">Nom</label>
+                            <input type="text" class="bg-[#1c1f27] border border-[#2a2d35] rounded text-white"></div>
+                    <div class="flex flex-col gap-1.5 min-w-50 w-1/3">
+                        <label for="" class="text-[#9ca3af] text-xs font-medium">Adresse e-mail</label>
+                        <input type="email" class="bg-[#1c1f27] border border-[#2a2d35] rounded text-white">
+                    </div>
+                    <div class="flex flex-col gap-1.5 min-w-50 w-1/3">
+                        <label for="" class="text-[#9ca3af] text-xs font-medium rounded">Mot de passe</label>
+                        <input type="password" class="bg-[#1c1f27] border border-[#2a2d35] rounded text-white">
+                    </div>
+                    <button class="text-white border border-[#2a2d35] p-3 w-1/3 rounded-md font-semibold hover:bg-[#22252f] hover:border-[#3a3d45]">Créer mon compte</button>
+                    <div class="flex flex-row mb-20 gap-1">
+                        <p class="text-[#9ca3af]">Deja un compte?</p>
+                        <a href="" class="text-[#e87722] underline">Se connecter</a>
+                    </div>
+                </div>
+            </form>
+        </section>
+    </div>
+</x-site-layout>
