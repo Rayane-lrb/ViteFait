03615d36e22e5dda513b771032970ec9e894fc7d
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-14 20:33:06 +0200

    STYLE: profile page styling

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,3 +1,22 @@
 <x-site-layout>
+    <section class="flex justify-center p-5">
+        <div class="flex flex-col items-center justify-center rounded-lg border border-gray-300 w-1/3 p-5">
+            <div class="flex justify-center border border-gray-300 rounded-full p-3 max-w-30">
+                <img src="{{$ownProfile->picture_path ?? asset('/images/person_icon.svg')}}" alt="">
+            </div>
+            <div class="self-start gap-1">
+                <div class="flex flex-row gap-2">
+                    <label class="text-[#9ca3af]">username:</label>
+                    <h3>{{$profile->username}}</h3>
+                </div>
+                <div class="flex flex-row gap-2">
+                    <label class="text-[#9ca3af]">name:</label>
+                    <p>{{$profile->user->name}}</p>
+                </div>
+                <p class="flex flex-row gap-1"><img src="{{asset('/images/calendar_icon.svg')}}" alt="">{{$profile->birthday->format('d-m-Y')}}</p>
+            </div>
 
+            <h5>{{$profile->bio ?? 'no bio yet'}}</h5>
+        </div>
+    </section>
 </x-site-layout>
