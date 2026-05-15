0e3f7d6a74b8f83ce56af68333b78db70fbac655
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-15 19:24:36 +0200

    STYLE: article image styling

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,18 +1,25 @@
 <x-site-layout>
-    <section>
-        <div class="border border-orange-300 m-4 rounded p-2">
-            <h2 class="font-bold text-2xl">{{$article->title}}</h2>
-            <h5 class="text-lg">{{$article->text}}</h5>
-            <div class="mt-auto flex justify-between items-center mt-4">
-                <div class="flex flex-row gap-1 text-md text-gray-400">
-                    <p>Writen by:</p>
-                    <a href="/profile/{{$authorProfile->id}}">
-                            <p class="underline hover:text-[#E06020]">{{$authorProfile->username}}</p>
-                    </a>
+    <section class="flex justify-center">
+        <div class="border border-orange-300 m-4 rounded p-3 w-3/4">
+            <div>
+                <h2 class="font-bold text-2xl mb-4">{{$article->title}}</h2>
+                @if($article->content_image)
+                    <img src="{{asset($article->content_image)}}" alt="article image" class="w-full h-[390px] object-cover max-h-auto rounded">
+                @endif
+                <div class="p-4">
+                    <h5 class="text-lg my-4">{{$article->text}}</h5>
+                    <div class="mt-auto flex justify-between items-center mt-4">
+                        <div class="flex flex-row gap-1 text-md text-gray-400">
+                            <p>Writen by:</p>
+                            <a href="/profile/{{$authorProfile->id}}">
+                                    <p class="underline hover:text-[#E06020]">{{$authorProfile->username}}</p>
+                            </a>
+                        </div>
+                        <span class="bg-[#E06020] text-white text-xs px-2 py-0.5 rounded-full">
+                                {{ $article->published_at->format('d M Y') }}
+                        </span>
+                    </div>
                 </div>
-                <span class="bg-[#E06020] text-white text-xs px-2 py-0.5 rounded-full">
-                        {{ $article->published_at->format('d M Y') }}
-                    </span>
             </div>
         </div>
     </section>
