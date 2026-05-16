f69c69163e1be2e35dc2c32371ca756a0a33bd70
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-16 09:39:00 +0200

    STYLE: add edit button to articles index

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,7 +2,15 @@
     <section class="flex justify-center">
         <div class="border border-orange-300 m-4 rounded p-3 w-3/4">
             <div>
-                <h2 class="font-bold text-2xl mb-4">{{$article->title}}</h2>
+                <div class="flex flex-row justify-between">
+                    <h2 class="font-bold text-2xl mb-4">{{$article->title}}</h2>
+                    <div class="flex flex-row gap-2 items-stretch mb-2">
+                        <a href="/admin/articles/{{$article->id}}/edit" class="border border-orange-400 px-2 text-orange-400 rounded hover:bg-orange-400 hover:text-white transition flex items-center">Éditer</a>
+                        <form action="" class="flex">
+                            <button class="bg-red-200 text-red-500 rounded p-2 hover:bg-[#ef4444] hover:text-white transition">Supprimer</button>
+                        </form>
+                    </div>
+                </div>
                 @if($article->content_image && $article->content_image !== 'null')
                     <img src="{{asset('storage/' . $article->content_image)}}" alt="article image" class="w-full h-[390px] object-cover max-h-auto rounded">
                 @endif
