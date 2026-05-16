6d7a587e17e3c61691f2504c0a7c002d367bb929
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-16 11:07:46 +0200

    FEATURE: error messages article edit

--- resources/views/admin/articles/edit.blade.php
+++ resources/views/admin/articles/edit.blade.php
@@ -8,14 +8,23 @@
                 <div class="flex flex-col">
                     <label for="title" class="font-semibold">Titre</label>
                     <input name="title" type="text" class="rounded" value="{{old('title', $article->title)}}">
+                    @error('title')
+                    <p class="text-red-400">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="flex flex-col">
                     <label for="text" class="font-semibold">Contenu</label>
                     <textarea name="text"  cols="60" rows="30" class="rounded">{{old('content', $article->text)}}</textarea>
+                    @error('text')
+                    <p class="text-red-400">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="flex flex-col">
                     <label for="content_image" class="font-semibold">Image</label>
                     <input name="content_image" type="file" class="rounded w-1/3 bg-orange-400 border border-[#2a2d35]">
+                    @error('content_image')
+                    <p class="text-red-400">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="ml-auto">
                     <a href="{{ route('articles.index') }}" class="bg-red-200 text-red-500 p-2 rounded-xl hover:bg-[#ef4444] hover:text-white transition">Annuler</a>
