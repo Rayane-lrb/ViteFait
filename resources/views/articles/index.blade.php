8be87c846259082373bdb6b3522203ed91f16445
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-16 11:39:27 +0200

    FEATURE: articles index anker to create

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,5 +1,8 @@
 <x-site-layout>
-    <h1 class="text-2xl font-bold p-5 text-orange-600">Articles</h1>
+    <div class="flex justify-between flex-row p-5">
+        <h1 class="text-2xl font-bold p-5 text-orange-600">Articles</h1>
+        <a href="admin/articles/create" class="bg-orange-500 text-white font-bold p-3 rounded-lg text-center flex items-center hover:bg-[#B04010]">+ Nouvel Article</a>
+    </div>
     <section class="grid grid-cols-4 gap-4 px-6 mb-2">
         @foreach($articles as $article)
             <a href="{{route('article.show', $article->id)}}" class="bg-white border border-orange-300 rounded-xl p-4 hover:scale-105 hover:shadow-lg transition flex flex-col h-44">
