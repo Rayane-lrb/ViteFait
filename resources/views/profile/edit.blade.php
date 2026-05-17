094debbee492d43aa4af6429782d5c3a9836131d
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-05-17 21:32:41 +0200

    FEATURE: picture input in profile edit

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,19 +4,31 @@
                 @csrf
                 @method('PUT')
                 <div class="flex flex-col items-start justify-center rounded-lg border border-gray-300 w-full max-w-md mx-auto p-4">
-                    <div class="flex justify-center border border-gray-300 rounded-full p-3 max-w-30 mb-4">
-                        <img src="{{$ownProfile->picture_path ?? asset('/images/person_icon.svg')}}" alt="">
-                    </div>
+                    <label for="picture_path" class="cursor-pointer mb-4">
+                        <div class="flex justify-center border border-gray-300 rounded-full p-3 max-w-30">
+                            <img src="{{$ownProfile->picture_path ?? asset('/images/person_icon.svg')}}" alt="">
+                        </div>
+                        <input id="picture_path" name="picture_path" type="file" accept=".jpeg,.jpg,.png" class="hidden">
+                        @error('picture_path')
+                        <p class="text-red-400">{{$message}}</p>
+                        @enderror
+                    </label>
                     <div class="self-start gap-1 w-full">
                         <div class="flex flex-col gap-2">
                             <label for="username" class="text-[#9ca3af]">username</label>
                             <input name="username" type="text" value="{{old('username', $profile->username)}}" class="h-8 rounded w-1/3">
+                            @error('username')
+                            <p class="text-red-400">{{$message}}</p>
+                            @enderror
                         </div>
                     </div>
                     <div class="flex flex-row gap-2 items-end w-full">
                         <div class="flex flex-col flex-1">
                             <label for="bio" class="text-[#9ca3af]">bio</label>
                             <input name="bio" type="text" value="{{old('bio', $profile->bio)}}" class="h-8 rounded w-full">
+                            @error('bio')
+                            <p class="text-red-400">{{$message}}</p>
+                            @enderror
                         </div>
                         <button type="submit" class="h-8 text-orange-400 bg-[#243447]/70 px-3 rounded-lg hover:text-orange-500 hover:bg-[#1b2a3e] transition mt-auto">Modifier</button>
                     </div>
